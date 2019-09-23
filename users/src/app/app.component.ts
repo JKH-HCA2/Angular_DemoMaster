3f60e34d0196ff59e43873dee0d11ab513f45fcb
Jeremy Han <jeremyhan94@gmail.com>
2019-09-23 13:08:39 -0400

    directive demo fin

--- users/src/app/app.component.ts
+++ users/src/app/app.component.ts
@@ -13,10 +13,32 @@ export class AppComponent {
   email: string = 'foobar@test.com';
   currentYear: number = 2019;
 
-  addNewUser: boolean = true;
+  addNewUser: boolean = false;
+  newUserAdded: boolean = false;
+
+  newUser: string = '';
 
   // declare a method
   getCurrentYear(): number {
     return this.currentYear;
   }
+
+  // executed when the Reset button is clicked
+  onReset(): void {
+    this.firstName = '';
+    this.lastName = '';
+    this.email = '';
+    this.addNewUser = false;
+    this.newUserAdded = false;
+  }
+
+  onAddUser(): void {
+    this.newUser = `${this.firstName} ${this.lastName} - Email: ${this.email}`;
+    this.addNewUser = true;
+    this.newUserAdded = true;
+  }
+  getColor(): string {
+    return this.newUserAdded === true ? '#000080' : '#FF0000'; // navy : red
+  }
+
 }
